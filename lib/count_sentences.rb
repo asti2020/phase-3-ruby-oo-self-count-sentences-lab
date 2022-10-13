2a06f0d9e4d6ee4c92cbe325f3cb0432badc43ad
Astika Luintel <astild2020@gmail.com>
2022-10-12 20:10:15 -0400

    commit tree 111d931bc5194a0b8dbbb76f53ec7d11998d394b

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,30 @@ require 'pry'
 class String
 
   def sentence?
-    
+    "#{self}"
+    "#{self}".end_with?(".")
   end
 
   def question?
-
+    "#{self}".end_with?("?")
   end
 
   def exclamation?
+    "#{self}".end_with?("!")
 
   end
-
+  binding.pry
   def count_sentences
+    sentence = []
+    if sentence.empty?
+      0
+      
+     elsif sentence == self
+      sentence.length
 
+     else
+      "hello world!" 
+     end    
   end
+
 end
